375bc1ed06b907cc91158f60dff11e0005e4fe1f
chacha86 <taejincha0619@gmail.com>
2026-07-30 11:48:45 +0900

    ddl-auto, show-sql

---
+++ src/main/java/com/back/p_62_260730/domain/wiseSaying/entity/WiseSaying.java
@@ -0,0 +1,20 @@
+package com.back.p_62_260730.domain.wiseSaying.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
+@Entity
+@NoArgsConstructor
+public class WiseSaying {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; // int
+    private String content; // varchar(255)
+    private String author; // varchar(255)
+
+}
