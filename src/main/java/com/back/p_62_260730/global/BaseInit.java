72cc459f177f876b370d31a15fd9bf4b01aa594c
chacha86 <taejincha0619@gmail.com>
2026-07-31 09:25:11 +0900

    show sql

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -1,15 +1,24 @@
 package com.back.p_62_260730.global;
 
+import com.back.p_62_260730.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // 빈 등록용
 public class BaseInit {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @Bean
     public ApplicationRunner init() {
         return args -> {
-            System.out.println("초기 데이터를 로딩합니다.");
+            System.out.println("post의 row 개수를 셉니다.");
+            postRepository.count();
+
+            // select count(*) from post;
         };
     }
 }
