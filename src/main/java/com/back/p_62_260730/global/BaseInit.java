29a9cb207b9b4ca45e61ab0e1c09a4f07574cf50
chacha86 <taejincha0619@gmail.com>
2026-07-31 09:38:47 +0900

    asni output, logging level

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -1,5 +1,6 @@
 package com.back.p_62_260730.global;
 
+import com.back.p_62_260730.domain.post.entity.Post;
 import com.back.p_62_260730.domain.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -17,8 +18,19 @@ public class BaseInit {
         return args -> {
             System.out.println("post의 row 개수를 셉니다.");
             postRepository.count();
-
             // select count(*) from post;
+
+
+            // post 하나 저장
+            Post post = new Post();
+            post.setTitle("제목1");
+            post.setBody("내용1");
+            postRepository.save(post);
+            // insert into post ...
+
+            // post 조회
+
+
         };
     }
 }
