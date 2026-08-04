54ef565d7d75240eee713da59f17465e58d61015
chacha86 <taejincha0619@gmail.com>
2026-08-04 09:39:07 +0900

    audit

--- src/main/java/com/back/p_62_260730/domain/post/entity/Post.java
+++ src/main/java/com/back/p_62_260730/domain/post/entity/Post.java
@@ -1,10 +1,12 @@
 package com.back.p_62_260730.domain.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,20 +14,28 @@ import java.time.LocalDateTime;
 @Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // int
     private String title; // varchar(255)
     private String body; // varchar(255)
+
+    @CreatedDate
     private LocalDateTime createDate;
+
+    @LastModifiedDate
     private LocalDateTime modifyDate;
 
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
-        this.createDate = LocalDateTime.now();
-        this.modifyDate = createDate;
+    }
+
+    public void modify(String title, String body) {
+        this.title = title;
+        this.body = body;
     }
 
 }
