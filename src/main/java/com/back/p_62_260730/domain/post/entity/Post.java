ab39e8736d0b52c8a46f8a75212160dd367bd682
chacha86 <taejincha0619@gmail.com>
2026-08-04 09:52:49 +0900

    baseEntity

--- src/main/java/com/back/p_62_260730/domain/post/entity/Post.java
+++ src/main/java/com/back/p_62_260730/domain/post/entity/Post.java
@@ -1,32 +1,19 @@
 package com.back.p_62_260730.domain.post.entity;
 
-import jakarta.persistence.*;
+import com.back.p_62_260730.global.BaseEntity;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Setter
 @Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
 @Entity
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id; // int
+public class Post extends BaseEntity {
+
     private String title; // varchar(255)
     private String body; // varchar(255)
-
-    @CreatedDate
-    private LocalDateTime createDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
 
     public Post(String title, String body) {
         this.title = title;
