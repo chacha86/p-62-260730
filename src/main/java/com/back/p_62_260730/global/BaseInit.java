da7c4d8573d9a576dd3271e982fdfdc4502a57b1
chacha86 <taejincha0619@gmail.com>
2026-08-04 10:07:30 +0900

    baseEntity abstract

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -72,6 +72,7 @@ public class BaseInit {
 
     @Transactional
     void work4() {
+
         Post post1 = postService.findById(1).get();
         postService.modify(post1, "제목1-수정4", "내용1-수정4");
     }
